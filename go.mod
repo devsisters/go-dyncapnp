e79abb16039082ecd7a9ecf8a4bd2014e9ca23e6
Kyungmin Bae <kyungmin.bae@devsisters.com>
2020-06-09 16:29:54 +0900

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/WKBae/go-dyncapnp
+
+go 1.14
---
+++ parser.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdlib.h>
+
+struct capnpFile {
+	const char* path;
+	char* content;
+	size_t contentLen;
+};
+
+void* parseSchemaFromFiles(const struct capnpFile* files, size_t filesLen, const char** paths, size_t pathsLen);
+
+void* findStructSchema(void* schemaPtr, char* name);
+
+void releaseSchemas(void* schemasPtr, size_t schemasLen);
+
+void releaseSchema(void* schemaPtr);
+
+#ifdef __cplusplus
+}
+#endif
