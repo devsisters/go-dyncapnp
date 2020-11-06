1deeb1479857ac8adf2c4093ec9a7382352abe21
WKBae <williambae1@gmail.com>
2020-11-06 16:02:36 +0900

    Cleanup parseSchemaFromFiles conversion code

--- cgo.go
+++ cgo.go
@@ -6,10 +6,6 @@ package dyncapnp
 
 #include "parser.h"
 #include <stdlib.h>
-
-struct capnpFile* allocFiles(size_t s) {
-	return (struct capnpFile*) malloc(sizeof(struct capnpFile) * s);
-}
 */
 import "C"
 import (
@@ -18,51 +14,23 @@ import (
 )
 
 func parseSchemaFromFiles(files map[string][]byte, imports map[string][]byte, paths []string) (unsafe.Pointer, []unsafe.Pointer, error) {
-	cFiles := C.allocFiles(C.size_t(len(files)))
-	defer C.free(unsafe.Pointer(cFiles))
+	cFiles, freeFiles := filesToCapnpFiles(files)
+	defer freeFiles()
 
-	cFileSlice := (*[1 << 30]C.struct_capnpFile)(unsafe.Pointer(cFiles))[:len(files):len(files)]
-	i := 0
-	for path, content := range files {
-		cFileSlice[i].path = C.CString(path)
-		cFileSlice[i].content = (*C.char)(unsafe.Pointer(&content[0]))
-		cFileSlice[i].contentLen = C.size_t(len(content))
-		i++
-	}
-	defer func() {
-		for _, f := range cFileSlice {
-			C.free(unsafe.Pointer(f.path))
-		}
-	}()
-
-	cImports := C.allocFiles(C.size_t(len(imports)))
-	defer C.free(unsafe.Pointer(cImports))
-
-	cImportSlice := (*[1 << 30]C.struct_capnpFile)(unsafe.Pointer(cImports))[:len(imports):len(imports)]
-	i = 0
-	for path, content := range imports {
-		cImportSlice[i].path = C.CString(path)
-		cImportSlice[i].content = (*C.char)(unsafe.Pointer(&content[0]))
-		cImportSlice[i].contentLen = C.size_t(len(content))
-		i++
-	}
-	defer func() {
-		for _, f := range cImportSlice {
-			C.free(unsafe.Pointer(f.path))
-		}
-	}()
+	cImports, freeImports := filesToCapnpFiles(imports)
+	defer freeImports()
 
 	cPathSlice := make([]*C.char, len(paths))
 	for i, path := range paths {
 		cPathSlice[i] = C.CString(path)
 	}
 	defer func() {
-		for _, chs := range cPathSlice {
-			C.free(unsafe.Pointer(chs))
+		for _, cPath := range cPathSlice {
+			C.free(unsafe.Pointer(cPath))
 		}
 	}()
 
-	res := C.parseSchemaFromFiles(cFiles, C.size_t(len(cFileSlice)), cImports, C.size_t(len(cImportSlice)), &cPathSlice[0], C.size_t(len(cPathSlice)))
+	res := C.parseSchemaFromFiles(cFiles, C.size_t(len(files)), cImports, C.size_t(len(imports)), &cPathSlice[0], C.size_t(len(cPathSlice)))
 	if res.err != nil {
 		err := fmt.Errorf(C.GoString(res.err))
 		C.free(unsafe.Pointer(res.err))
@@ -78,6 +46,26 @@ func parseSchemaFromFiles(files map[string][]byte, imports map[string][]byte, pa
 	C.free(unsafe.Pointer(res.schemas))
 
 	return res.parser, ptrs, nil
+}
+
+func filesToCapnpFiles(files map[string][]byte) (*C.struct_capnpFile, func()) {
+	cFiles := (*C.struct_capnpFile)(C.malloc(C.sizeof_struct_capnpFile * C.size_t(len(files))))
+
+	cFileSlice := (*[1 << 30]C.struct_capnpFile)(unsafe.Pointer(cFiles))[:len(files):len(files)]
+	i := 0
+	for path, content := range files {
+		cFileSlice[i].path = C.CString(path)
+		cFileSlice[i].content = (*C.char)(unsafe.Pointer(&content[0]))
+		cFileSlice[i].contentLen = C.size_t(len(content))
+		i++
+	}
+
+	return cFiles, func() {
+		for _, f := range cFileSlice {
+			C.free(unsafe.Pointer(f.path))
+		}
+		C.free(unsafe.Pointer(cFiles))
+	}
 }
 
 func findNested(ptr unsafe.Pointer, name string) (unsafe.Pointer, error) {
